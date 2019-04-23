457691d45a1f42fa9e8ca5d7549c94df4b9e52d6
Chris Shaffer <chris@methodbox.io>
2019-04-23 14:54:52 -0500

    updated README, added tslint with plugin for better linting

--- app.ts
+++ app.ts
@@ -4,5 +4,5 @@ let notFalse: boolean = true;
 let anArray: Array<any> = ['this can have anything', 12, false, true, {}];
 let someTuple: [string, number, boolean, any] = ['string', 10, true, {}];
 // prettier-ignore
-enum pizza {'Pepperoni', 'Cheese', 'Bacon'}
+enum pizza { 'Pepperoni', 'Cheese', 'Bacon' }
 let anyThing: any = 'This is a string but it could be anything like a' + 10;
