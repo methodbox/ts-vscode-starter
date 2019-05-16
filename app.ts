7a9cac4dd8494a96363dedc5c1f68f7e8764ffee
Chris Shaffer <chris@methodbox.io>
2019-05-15 19:36:53 -0500

    updated tsconfig rules, removed example app

--- app.ts
+++
@@ -1,8 +0,0 @@
-let hello: string = 'Hello, World.';
-let num: number = 20;
-let notFalse: boolean = true;
-let anArray: Array<any> = ['this can have anything', 12, false, true, {}];
-let someTuple: [string, number, boolean, any] = ['string', 10, true, {}];
-// prettier-ignore
-enum pizza { 'Pepperoni', 'Cheese', 'Bacon' }
-let anyThing: any = 'This is a string but it could be anything like a' + 10;
